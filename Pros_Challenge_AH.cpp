aba12e2a417ff5e3a11236115948c05fc4f40bb0
anthony-hasan <117035438+anthony-hasan@users.noreply.github.com>
2022-10-30 12:54:49 -0500

    Add files via upload

---
+++ Pros_Challenge_AH.cpp
@@ -0,0 +1,18 @@
+//best time to buy a product
+// use historical data to predict future price of product and be able to tell when the most optimal time to buy product is
+// this will be useful to groups seeking to make large burchases where evey advantage helps, in fields as varied as munifacturing, to agriculture and even supply chain mangemnt
+// way to do this is by gathering data set, applying a 
+
+// Random Forest and five different ensemble methods such as Adaptive
+// Boosting (AdaBoost), GredientBoost,eXtreme Gradient Boosting (XGBoost), Bagging, LighGBM (Light Gradient Boosted
+// Machine) were used to improve upon the baseline model.
+// Ensemble methods are chosen for this experiment because they
+// are proven to perform well from small to medium seized data
+// set.
+
+// this is all from this paper "Predicting Price of Daily Commodities using "Machine Learning"
+// https://doi.org/10.1109/3ICT51146.2020.9312012
+
+
+// this should do the job
+// all that's left is testing and making the app
